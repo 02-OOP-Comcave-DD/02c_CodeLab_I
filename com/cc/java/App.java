e8f47036059cd67ff2b49bdeb620e437bd4bf85c
René <media-r.r@online.de>
2024-05-23 15:41:29 +0200

    01 / Klasse Konto angelegt | int Variablem, Wert 10000 | 3 KontoInstanzen angelegt

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,6 +4,21 @@ public class App {
     
     public static void main(String[] args) {
     
+    // Erzeugen von 3 Konto-Instanzen \\
+    Konto konto1 = new Konto();
+    Konto konto2 = new Konto();
+    Konto konto3 = new Konto();
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
---
+++ com/cc/java/Konto.java
@@ -0,0 +1,17 @@
+package com.cc.java;
+
+public class Konto {
+
+    // eine "int" Variable "kontostand" mit dem Wert 10000 angelegt \\
+    private int kontostand = 10000;
+
+
+
+
+
+public int getKontostand() {
+    return kontostand;
+}
+
+
+}
