5d903d47b197d14e4ae941d16e6f335caede4acf
René <media-r.r@online.de>
2024-05-23 16:02:49 +0200

    03 / Methoden zur vervielfachung

--- com/cc/java/Konto.java
+++ com/cc/java/Konto.java
@@ -5,6 +5,21 @@ public class Konto {
     // eine "int" Variable "kontostand" mit dem Wert 10000 angelegt \\
     private int kontostand = 10000;
 
+    
+    // Methode den Kontostand zu verdoppeln \\
+    public void verdoppeln() {
+        kontostand *= 2;
+    }
+
+    // Methode den Kontostand zu verdreifachen \\
+    public void verdreifachen() {
+        kontostand *= 3;
+    }
+
+    // Methode den Kontostand zu verzehnfachen \\
+    public void verzehnfachen() {
+        kontostand *= 10;
+    }
 
 
 
